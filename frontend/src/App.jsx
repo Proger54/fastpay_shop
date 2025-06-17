2d36b5bf2bc1b014ed4cc3225ce9855599c82a6c
Proger54 <xxrindsxx@gmail.com>
2025-06-17 22:25:09 +0700

    [main] update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,38 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import LoginPage from "./LoginPage";
+import HomePage from "./HomePage";
+import ProfilePage from "./ProfilePage";
+import NotFound404 from "./NotFound404";
 
-import NotFound404 from './Pages/NotFound404/NotFound404';
-import LoginPage from './Pages/Login/LoginPage';
-import HomePage from './Pages/Home/HomePage';
-import ProfilePage from './Pages/Profile/ProfilePage';
-
-import './App.css';
-
-function App() {
-
-  const navigate = useNavigate()
+function AppContent() {
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const user_token = localStorage.getItem("sp_token");
-    if (!user_token) {
+    
+    if (!user_token && location.pathname !== "/login") {
       navigate("/login");
     }
-  }, [])
+  }, [location.pathname, navigate]);
 
   return (
+    <div className="App">
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path="*" element={<NotFound404 />} />
+      </Routes>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/' element={<HomePage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-          <Route path="*" element={<NotFound404 />} />
-        </Routes>
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
